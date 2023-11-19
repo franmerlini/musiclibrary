2b5cb5752bbc9c37e4e7a3be24cf467d5c4029ac
Francisco Merlini <merlinifrancisco@gmail.com>
2023-11-19 17:00:40 -0300

    initial commit

---
+++ src/test/java/com/merlini/musiclibrary/MusiclibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.merlini.musiclibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusiclibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
